a37cffd6bc72339e23aae8cef60765243ae82602
Tathagata <tathagata@conn3cted.com>
2022-05-06 10:48:14 +1000

    adding extra fields to Brand and Card

--- src/Models/Brand.js
+++ src/Models/Brand.js
@@ -3,11 +3,13 @@ export class Brand {
     name;
     locations =new Array();
     terms_of_use;
+    card_terms_and_conditions;
 
-    constructor(identifier, name, locations, terms_of_use) { 
+    constructor(identifier, name, locations, terms_of_use, card_terms_and_conditions) { 
        this.identifier = identifier;
        this.name = name;
        this.locations = locations;
        this.terms_of_use = terms_of_use;
+       this.card_terms_and_conditions = card_terms_and_conditions;
      }
   }
--- src/Models/Card.js
+++ src/Models/Card.js
@@ -8,8 +8,12 @@ export class Card {
     image;
     private_callback_url;
     private_token_id;
+    created_at;
+    type;
+    status;
 
-     constructor(card_id, amount, delivery_email, delivery_sms_number, expiry_date, card_design_id, image, private_callback_url, private_token_id) { 
+
+     constructor(card_id, amount, delivery_email, delivery_sms_number, expiry_date, card_design_id, image, private_callback_url, private_token_id, created_at, type, status) { 
         this.card_id = card_id;
         this.amount = amount;
         this.delivery_email = delivery_email;
@@ -19,6 +23,9 @@ export class Card {
         this.expiry_date = expiry_date;
         this.private_callback_url = private_callback_url;
         this.private_token_id = private_token_id;
+        this.created_at = created_at;
+        this.type = type;
+        this.status = status;
  
       }
    }

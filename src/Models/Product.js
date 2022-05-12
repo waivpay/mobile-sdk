bf9d851087a67c8c5aea901dce9d944f480fe8e7
Tathagata <tathagata@conn3cted.com>
2022-05-13 09:53:31 +1000

    adding new fields to product model in catalogue
    physical, fee

--- src/Models/Product.js
+++ src/Models/Product.js
@@ -2,21 +2,25 @@ export class Product {
   id;
   name;
   active;
+  physical;
   digital;
   description;
   image;
+  fee;
   digital_fee;
   minimum_card_value;
   maximum_card_value;
 
 
-  constructor(id, name, active, digital, description, image, digital_fee, minimum_card_value, maximum_card_value) {
+  constructor(id, name, active, physical, digital, description, image, fee,  digital_fee, minimum_card_value, maximum_card_value) {
     this.id = id;
     this.name = name;
     this.active = active;
+    this.physical = physical;
     this.digital = digital;
     this.description = description;
     this.image = image;
+    this.fee = fee;
     this.digital_fee = digital_fee;
     this.minimum_card_value = minimum_card_value;
     this.maximum_card_value = maximum_card_value;

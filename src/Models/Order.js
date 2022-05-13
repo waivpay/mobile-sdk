b6b47b46039b1a08698851d8bc8f7b5622f7e489
Tathagata <tathagata@conn3cted.com>
2022-05-13 11:40:55 +1000

    addition of fields related to order
    payment_token, include_message, delivery_first_name, delivery_last_name

--- src/Models/Order.js
+++ src/Models/Order.js
@@ -13,11 +13,12 @@ export class Order {
   credit_card_expiry_month;
   credit_card_expiry_year;
   credit_card_security_code;
+  payment_token;
   billing_address = new BillingAddress();
   order_items = new Array();
 
 
-  constructor(email, phone_number, user_id, external_user_id, billing_same_address, recipient_address, credit_card_number, credit_card_name, credit_card_expiry_month, credit_card_expiry_year, credit_card_security_code, billing_address, order_items) {
+  constructor(email, phone_number, user_id, external_user_id, billing_same_address, recipient_address, credit_card_number, credit_card_name, credit_card_expiry_month, credit_card_expiry_year, credit_card_security_code, payment_token, billing_address, order_items) {
     this.email = email;
     this.phone_number = phone_number;
     this.user_id = user_id;
@@ -27,6 +28,7 @@ export class Order {
     this.credit_card_expiry_month = credit_card_expiry_month;
     this.credit_card_expiry_year = credit_card_expiry_year;
     this.credit_card_security_code = credit_card_security_code;
+    this.payment_token = payment_token;
     this.billing_address = billing_address;
     this.credit_card_name = credit_card_name;
     this.order_items = order_items;

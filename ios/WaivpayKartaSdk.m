ecb0a6f525d9137f8fa2275eeb508f2b4d1455d6
Kelly Lamborn <kelly@conn3cted.com>
2022-05-18 15:38:22 +1000

    Add to wallet

--- ios/WaivpayKartaSdk.m
+++ ios/WaivpayKartaSdk.m
@@ -2,7 +2,7 @@
 
 @interface RCT_EXTERN_MODULE(WaivpayKartaSdk, NSObject)
 
-RCT_EXTERN_METHOD(addCard:(String)cardId withB:(String)cardHolder
+RCT_EXTERN_METHOD(addCard:(NSString *)cardId withB:(NSString *)cardHolder
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 

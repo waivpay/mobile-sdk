e23edb523103980ee7ccfdbe1aad8a2340fdefbe
Kelly Lamborn <kelly@conn3cted.com>
2022-06-24 09:52:37 +1000

    Updates to the SDK

--- android/src/main/java/com/waivpaykartasdk/WaivpayKartaSdkModule.java
+++ android/src/main/java/com/waivpaykartasdk/WaivpayKartaSdkModule.java
@@ -1,5 +1,7 @@
 package com.waivpaykartasdk;
 
+import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -36,8 +38,13 @@ public class WaivpayKartaSdkModule extends ReactContextBaseJavaModule {
     public void addCard(String cardId, String cardSuffix, String cardHolder, String env, String deliveryEmail, String appId, String accessToken, Promise promise) {
         Log.e("KLHERE","addCard");
         try {
+
+//            Context context = getReactApplicationContext();
+//            Intent intent = this.intentWithContentUri(uri, PKPASS_TYPE);
+//            context.startActivity(intent);
+
             AddToWallet addToWallet = new AddToWallet();
-            addToWallet.addCardToWallet(cardId, cardSuffix, cardHolder, env, deliveryEmail, appId, accessToken, this.getCurrentActivity());
+            addToWallet.addCardToWallet(cardId, cardSuffix, cardHolder, env, deliveryEmail, appId, accessToken, getCurrentActivity());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -45,4 +52,27 @@ public class WaivpayKartaSdkModule extends ReactContextBaseJavaModule {
     }
 
     public static native boolean nativeAddCard(String cardId, String cardSuffix, String cardHolder, String env, String deliveryEmail, String appId, String accessToken);
+
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == REQUEST_CODE_PUSH_TOKENIZE) {
+//            if (resultCode == RESULT_CANCELED) {
+//                // TODO: Handle provisioning failure here.
+//                return;
+//            } else if (resultCode == RESULT_OK) {
+//                // TODO: Handle successful provisioning here.
+//                String tokenId = data.getStringExtra(TapAndPay.EXTRA_ISSUER_TOKEN_ID);
+//                return;
+//            }
+//        }
+//        // TODO: Handle results for other request codes.
+//        // ...
+//    }
+
+//    private Intent intentWithContentUri(Uri uri, String type) {
+//        return new Intent(Intent.ACTION_VIEW)
+//                .setDataAndType(uri, type)
+//                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//                .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+//    }
 }

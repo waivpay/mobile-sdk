f5d6ce46b60a2baafcf24e5b7165f8169938e7ee
Tathagata <tathagata@conn3cted.com>
2022-08-15 15:51:18 +1000

    prod encryption key eway

--- src/util/ServerEndPoints.js
+++ src/util/ServerEndPoints.js
@@ -28,5 +28,5 @@ export const EndPointsCashBack = {
 
 export const Eway = {
   encryptionKeyStaging : 'q4QeqY+qhXgaewGiXW/C4bMcTgG+ggH1rzV+8bFG4+pE95CDHwh2vcW1J4tdtVI4dhdejqw9gyh1NfEPG/KZsDPGkSrDdh0qgLIQ3ydozrSL2M5g67wNV/sCY4xn/a3GAso1rX4rLD95W36OnCl96ED7uZn5/kTUdq/3P2z6R/Sd8pRaPw5MacTEHeNuT6fAwtH0AVRR+vHwnDWVVn7HRJVWuYta0qgznQwkpYkdoahQ6uJ62rl4gF19YKYv+fdHFOtV6sujQuTKeXwNcwUM7ssZYAwsEod/jzC4uoeop0nZ0+43oKsHgU0yGSqBad9DQHdG0r6fOD+JYcBn+ReE9Q==',
-  encryptionKeyProd : 'toBeProvided'
+  encryptionKeyProd : 'oESsvfHyTHokeh0rhKpcjP3eqFgkv+4uRcRvC720kyvQGdD+nqPZWfKKqMuyUGmlaIEPXDzfQO1BULkFKxSM7KKBCiVahPSsf8Qz4/GoY3JlP+iOF6V1FkpqnWynKzwjFCzdb2n7cpEMRyfjtdvPOP47I2l4vqt1mMD4Chvjdm4/nhQ9Um63B5Z9hvifrg5ghPWgGXGksc/L24JyI4SKwJhKvErNL1N9CD1wdbVGEM0FyyOHEQSbLlcYFe0NReye7udlbAa6mk9sy2hp/hKIbEZQ8J5EVZ2ddzXK0b/YCAUrpexBouNv0m3RaD7rxF3hvn1GSCsEFJYYLBkVdDzJ8w=='
 };
